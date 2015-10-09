85d1391223066124bc54564664adb4253dba0d17
Kevin <kevin.denver@gmail.com>
2015-10-09 11:31:47 +0100

    feat(hystrix): increased timeout

--- src/main/java/com/lv/springboot/externals/DuckDuckGoApi.java
+++ src/main/java/com/lv/springboot/externals/DuckDuckGoApi.java
@@ -4,6 +4,7 @@ import com.lv.springboot.externals.ex.DuckDuckGoException;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -23,7 +24,11 @@ public class DuckDuckGoApi {
     @Autowired @Value("${duckduckgo.url}")
     private String url;
 
-    @HystrixCommand(fallbackMethod = "fallback")
+    @HystrixCommand(
+        fallbackMethod = "fallback",
+        commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
+        })
     public Map zeroClickInfo(String q) {
         final HttpResponse<Map> response = callAsync(Map.class, () -> Unirest.get(format(url, q))
             .header("X-Mashape-Key", mashapeKey)
