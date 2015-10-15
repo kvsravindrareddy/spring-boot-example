67c0a0eb2f520003c3292eba13995facd0d0e331
Kevin <kevin.denver@gmail.com>
2015-10-15 16:12:03 +0100

    feat(rest-assured): added rest-assured for API tests

--- src/test/java/api/UserResourceApiTest.java
+++ src/test/java/api/UserResourceApiTest.java
@@ -1,19 +1,20 @@
 package api;
 
 import com.github.toastshaman.springboot.Application;
+import com.jayway.restassured.response.Response;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.http.ContentType.JSON;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.entry;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static util.TestData.normalUserKevin;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -22,20 +23,28 @@ import static util.TestData.normalUserKevin;
 @ActiveProfiles("local")
 public class UserResourceApiTest {
 
-    private final RestTemplate restTemplate = new TestRestTemplate();
-
     @Test
     @SuppressWarnings("unchecked")
     public void savesUsers() {
-        final Map response = restTemplate.postForObject("http://localhost:9091/users", normalUserKevin(), Map.class);
-        assertThat(response).contains(entry("status", "ok"));
+        given().contentType(JSON).body(normalUserKevin()).post("http://localhost:9091/users").then()
+            .assertThat()
+            .body("status", equalTo("ok"));
     }
 
     @Test
     @SuppressWarnings("unchecked")
     public void returnsUsers() {
-        restTemplate.postForObject("http://localhost:9091/users", normalUserKevin(), Map.class);
-        final Map response = restTemplate.getForObject("http://localhost:9091/users/1", Map.class);
-        assertThat(response).contains(entry("firstname", "Kevin"), entry("lastname", "Denver"));
+        insert(normalUserKevin());
+
+        given().contentType(JSON).get("http://localhost:9091/users/1").then()
+            .assertThat()
+            .body("firstname", equalTo("Kevin"))
+            .body("lastname", equalTo("Denver"));
+    }
+
+    private void insert(Map user) {
+        final Response response = given().contentType("application/json").body(user)
+            .post("http://localhost:9091/users").andReturn();
+        assertThat(response.getStatusCode()).isEqualTo(200);
     }
 }
